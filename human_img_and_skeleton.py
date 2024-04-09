b09fd85c7f80e525cd9324d2e52b3c399e952456
Pacheco Melanie <melanie.pacheco@student-cs.fr>
2024-04-09 14:41:19 +0200

    Remove human_img_and_skeleton.py from repository

--- human_img_and_skeleton.py
+++
@@ -1,7 +0,0 @@
-def show_human_and_skeleton(img, human_skeleton_img):
-    print("This is a human and its skeleton")
-    print("    O    ")
-    print("   /|\   ")
-    print("   / \   ")
-    
-    return None
